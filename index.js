f2677315a91348b31500b839a508fb070f56bc55
Kayla Thomas <kthomas2478@gmail.com>
2020-08-23 16:00:28 -0400

    popup js

---
+++ index.js
@@ -0,0 +1,33 @@
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close-button");
+const saveButton = document.querySelector(".form__save-button");
+const popup = document.querySelector(".popup");
+const form = document.querySelector(".form");
+
+function togglePopup() {
+  popup.classList.toggle("popup_opened");
+}
+
+editButton.addEventListener("click", togglePopup);
+saveButton.addEventListener("click", togglePopup);
+
+closeButton.addEventListener("click", () => {
+  togglePopup();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputName = document.querySelector(".form__input-name");
+  const inputDescription = document.querySelector(".form__input-description");
+
+  const profileName = document.querySelector(".profile__info-name");
+  const profileDescription = document.querySelector(
+    ".profile__info-description"
+  );
+
+  profileName.textContent = inputName.value;
+  profileDescription = inputDescription.value;
+
+  togglePopup();
+});
