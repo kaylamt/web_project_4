a1f39d8e089b69c2844b9b0c118854466a5f5d97
Kayla Thomas <kthomas2478@gmail.com>
2020-08-26 19:24:28 -0400

    review changes

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
 const form = document.querySelector(".form");
-const inputName = document.querySelector(".form__input_name");
-const inputDescription = document.querySelector(".form__input_description");
+const inputName = document.querySelector(".form__input_field-name");
+const inputDescription = document.querySelector(
+  ".form__input_field-description"
+);
 const profileName = document.querySelector(".profile__info-name");
 const profileDescription = document.querySelector(".profile__info-description");
 
@@ -20,6 +22,7 @@ editButton.addEventListener("click", togglePopup);
 closeButton.addEventListener("click", togglePopup);
 
 function submitValue() {
+  submitValue.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
 
