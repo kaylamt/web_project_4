4164dd7070f5eec93fff950cf32c0b0aab4424e3
Kayla Thomas <kthomas2478@gmail.com>
2020-08-25 22:34:46 -0400

    review changes

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
 const form = document.querySelector(".form");
+const inputName = document.querySelector(".form__input_name");
+const inputDescription = document.querySelector(".form__input_description");
+const profileName = document.querySelector(".profile__info-name");
+const profileDescription = document.querySelector(".profile__info-description");
 
 function togglePopup() {
   popup.classList.toggle("popup_opened");
+  if (popup.classList.contains("popup_opened")) {
+    inputName.value = profileName.textContent;
+    inputDescription.value = profileDescription.textContent;
+  }
 }
 
 editButton.addEventListener("click", togglePopup);
 
 closeButton.addEventListener("click", togglePopup);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const inputName = document.querySelector(".form__input-name");
-  const inputDescription = document.querySelector(".form__input-description");
-
-  const profileName = document.querySelector(".profile__info-name");
-  const profileDescription = document.querySelector(
-    ".profile__info-description"
-  );
-
+function submitValue() {
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
 
   togglePopup();
-});
+}
+
+form.addEventListener("submit", submitValue);
