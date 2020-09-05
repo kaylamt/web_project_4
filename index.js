e391942b757db6a3390d11481fc346cb1eb6e1c3
Kayla Thomas <kthomas2478@gmail.com>
2020-09-05 17:48:27 -0400

    fixed delete button

--- index.js
+++ index.js
@@ -84,6 +84,33 @@ formAdd.addEventListener("submit", submitValueAdd);
 //   },
 // ];
 
+// const initialCards = [
+//   {
+//     name: "Yosemite Valley",
+//     link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+//   },
+//   {
+//     name: "Lake Louise",
+//     link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+//   },
+//   {
+//     name: "Bald Mountains",
+//     link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+//   },
+//   {
+//     name: "Latemar",
+//     link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+//   },
+//   {
+//     name: "Vanoise National Park",
+//     link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+//   },
+//   {
+//     name: "Lago di Braies",
+//     link: "https://code.s3.yandex.net/web-code/lago.jpg"
+//   }
+// ];
+
 // initialCards.forEach(data => {
 //   const cardTemplate = document.querySelector(".card-template").content.querySelector(".card");
 //   const cardElement = cardTemplate.cloneNode(true);
@@ -92,7 +119,11 @@ formAdd.addEventListener("submit", submitValueAdd);
 //   const cardTitle = cardElement.querySelector(".card__text");
 
 //   cardTitle.textContent = data.name;
-//   // cardImage.style.backgroundImage = `url(${data.link})`;
+//   cardImage.style.backgroundImage = `url(${data.link})`;
 
-//   cardImage.src = data.link;
+//   const cards = document.querySelector(".cards");
+//   cards.prepend();
+
 // });
+
+
